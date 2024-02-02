ec64b261bc126e93f807fc34ad7ea635eba9da31
ppzxc <cjh8487@naver.com>
2024-02-02 23:36:33 +0900

    2024-02-02 variable, shadowing

---
+++ variables/src/main.rs
@@ -0,0 +1,12 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+fn main() {
+    // variables
+    let mut x = 5;
+    println!("The Value Of x is: {x}");
+    x = 6;
+    println!("The Value Of x is: {x}");
+
+    // constant
+    println!("Value: {THREE_HOURS_IN_SECONDS}")
+}
