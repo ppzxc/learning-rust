5ee198d915b11e890b99b744ad9d7ffea24d7551
ppzxc <cjh8487@naver.com>
2025-08-11 17:55:37 +0900

    2025-08-11 add

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    string();
+    memory_and_allocation();
+}
+
+fn string() {
+    // let s = String::from("hello");
+    let mut s = String::from("hello");
+    s.push_str(", world!"); // push_str() appends a literal to a String
+    println!("{s}"); // this will print `hello, world!`
+}
+
+fn memory_and_allocation() {
+    let x = 5;
+    let y = x;
+
+    let s1 = String::from("hello");
+    let s2 = s1;
 }
